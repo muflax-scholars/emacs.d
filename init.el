03bf89726a351b510da62915e7c03c87f5dbcc50
muflax <mail@muflax.com>
2011-01-29 15:10:45 +0100

    different haskell indent mode

--- init.el
+++ init.el
@@ -431,5 +431,4 @@
 ;; haskell mode
     (load "~/.emacs.d/site-lisp/haskell-mode/haskell-site-file")
     (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-    (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
-
+    (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
