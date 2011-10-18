be62854872ec2936c0cbd3f442d43c4641ebfa59
muflax <mail@muflax.com>
2011-10-18 10:47:57 +0200

    move lines

--- init.el
+++ init.el
@@ -704,6 +704,34 @@
 (global-set-key "\C-cD" 'surround-delete-within)
 (global-set-key "\C-c\M-d" 'surround-change)
 
+;; move lines like in org-mode
+(defun move-line (n)
+  "Move the current line up or down by N lines."
+  (interactive "p")
+  (setq col (current-column))
+  (beginning-of-line) (setq start (point))
+  (end-of-line) (forward-char) (setq end (point))
+  (let ((line-text (delete-and-extract-region start end)))
+    (forward-line n)
+    (insert line-text)
+    ;; restore point to original column in moved line
+    (forward-line -1)
+    (forward-char col)))
+
+(defun move-line-up (n)
+  "Move the current line up by N lines."
+  (interactive "p")
+  (move-line (if (null n) -1 (- n))))
+
+(defun move-line-down (n)
+  "Move the current line down by N lines."
+  (interactive "p")
+  (move-line (if (null n) 1 n)))
+
+(global-set-key (kbd "M-<up>")   'move-line-up)
+(global-set-key (kbd "M-<down>") 'move-line-down)
+
+
 ;; move buffers
 (require 'buffer-move)
 (global-set-key (kbd "<C-S-up>")    'buf-move-up)
