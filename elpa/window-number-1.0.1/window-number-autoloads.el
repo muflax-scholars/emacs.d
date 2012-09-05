00b62028f5e74bab6397f0193a09250acc5a5156
muflax <mail@muflax.com>
2012-09-05 23:53:42 +0200

    emacs package update

---
+++ elpa/window-number-1.0.1/window-number-autoloads.el
@@ -0,0 +1,18 @@
+;;; window-number-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("window-number-pkg.el" "window-number.el")
+;;;;;;  (20551 51546 567975))
+
+;;;***
+
+(provide 'window-number-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; window-number-autoloads.el ends here
