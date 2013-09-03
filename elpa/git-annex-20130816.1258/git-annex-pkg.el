e2dcb0eb9ceafbc062161dab78271407d9bc106d
muflax <mail@muflax.com>
2013-09-03 03:42:30 +0200

    emacs package upgrade

---
+++ elpa/git-annex-20130816.1258/git-annex-pkg.el
@@ -0,0 +1,1 @@
+(define-package "git-annex" "20130816.1258" "Mode for easy editing of git-annex'd files [github]" (quote nil))
--- elpa/hi2-20130809.1253/hi2-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "hi2" "20130809.1253" "indentation module for Haskell Mode [github]" (quote nil))
