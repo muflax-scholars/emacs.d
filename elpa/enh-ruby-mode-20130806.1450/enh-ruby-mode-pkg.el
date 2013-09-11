e7fdf7c5ba2a3bdc9e69ca5576df2dbef6175661
muflax <mail@muflax.com>
2013-09-11 17:43:36 +0200

    emacs package update

--- elpa/enh-ruby-mode-20130806.1450/enh-ruby-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-(define-package "enh-ruby-mode" "20130806.1450" "Major mode for editing Ruby files [github]" 'nil)
-
---
+++ elpa/hi2-20130903.1912/hi2-pkg.el
@@ -0,0 +1,1 @@
+(define-package "hi2" "20130903.1912" "indentation module for Haskell Mode [github]" (quote nil))
