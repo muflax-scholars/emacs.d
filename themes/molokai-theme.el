ca8a1f5d0c3ab1860f421165ad38fa384b12c55f
muflax <mail@muflax.com>
2013-12-27 17:32:03 +0000

    work around yet another fucking defthemes bug

--- themes/molokai-theme.el
+++ themes/molokai-theme.el
@@ -1,28 +1,18 @@
-;;; molokai-theme.el --- molokai theme with Emacs theme engine
-
-;; Copyright (C) 2013 by Adam Lloyd
-;; Copyright (C) 2013 by Syohei YOSHIDA
-
-;; Author: Syohei YOSHIDA <syohex@gmail.com>
-;; URL: https://github.com/alloy-d/color-theme-molokai
-;; Version: 20130828.1600
-;; X-Original-Version: 0.01
-
+;;; molokai-theme.el --- based on various molokai themes
+;;
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
-
+;;
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-
+;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-;;; Commentary:
-
+;;
 ;;; Code:
 
 (deftheme molokai
--- themes/twilight-anti-bright-theme.el
+++ themes/twilight-anti-bright-theme.el
@@ -1,56 +1,17 @@
-;;; twilight-anti-bright-theme.el --- A soothing Emacs 24 light-on-dark theme
+;;; twilight-anti-bright-theme.el --- based on twilight-anti-bright
 ;;
-;; Copyright (c) 2012 Jim Myhrberg.
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
 ;;
-;; Author: Jim Myhrberg <contact@jimeh.me>
-;; Version: 20120713.1116
-;; X-Original-Version: 0.2.0
-;; Keywords: themes
-;; URL: https://github.com/jimeh/twilight-anti-bright-theme.el
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
 ;;
-;; This file is NOT part of GNU Emacs.
-;;
-;;; License:
-;;
-;; Permission is hereby granted, free of charge, to any person obtaining a
-;; copy of this software and associated documentation files (the "Software"),
-;; to deal in the Software without restriction, including without limitation
-;; the rights to use, copy, modify, merge, publish, distribute, sublicense,
-;; and/or sell copies of the Software, and to permit persons to whom the
-;; Software is furnished to do so, subject to the following conditions:
-;;
-;; The above copyright notice and this permission notice shall be included in
-;; all copies or substantial portions of the Software.
-;;
-;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-;; FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-;; DEALINGS IN THE SOFTWARE.
-;;
-;;; Credit:
-;;
-;; This is a light-on-dark theme inspired by the dark-on-light Twilight Bright
-;; TextMate theme by Florian Pichler.
-;;
-;;; Commentary:
-;;
-;; I particularly like the colors used in Twilight Bright, but I prefer
-;; light-on-dark themes, so I set out to create such a theme inspired by
-;; Twilight Bright. This is the result :)
-;;
-;;; Change Log:
-;;
-;; 2012-07-13 (0.2.0):
-;;    Make default and comment foreground colors a bit brighter.
-;;
-;; 2012-07-03 (0.1.1):
-;;    Make ECB foreground text colors a bit brighter.
-;;
-;; 2012-06-30 (0.1.0):
-;;    Initial release.
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ;;
 ;;; Code:
 
