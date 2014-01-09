a37a284d2f6fa167c618e1f573773a996961d2aa
muflax <mail@muflax.com>
2014-01-09 09:28:58 +0000

    meh

--- elpa/steady-theme-0.1.0/steady-theme-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; steady-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("steady-theme-pkg.el" "steady-theme.el")
-;;;;;;  (21198 27278 120941 589000))
-
-;;;***
-
-(provide 'steady-theme-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; steady-theme-autoloads.el ends here
--- elpa/steady-theme-0.1.0/steady-theme-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "steady-theme" "0.1.0" "A steady theme for Emacs  -*- lexical-binding: t -*-" (quote nil))
