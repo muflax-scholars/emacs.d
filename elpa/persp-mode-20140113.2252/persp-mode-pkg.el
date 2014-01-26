7466a03117431b458313df9a5ce0cfe4cde593a2
muflax <mail@muflax.com>
2014-01-26 09:34:20 +0000

    package update

--- elpa/persp-mode-20140113.2252/persp-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "persp-mode" "20140113.2252" "\"perspectives\" + save/load + shared among frames - bugs." (quote ((workgroups "0.2.0"))))
--- elpa/racket-mode-20140108.1753/racket-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "racket-mode" "20140108.1753" "Major mode for Racket language." 'nil)
