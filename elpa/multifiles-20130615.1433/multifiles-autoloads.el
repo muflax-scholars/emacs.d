53a6d420793e1d3b5f4a37b1a2d71dac02adc014
muflax <mail@muflax.com>
2014-02-18 03:00:28 +0000

    multifile package

---
+++ elpa/multifiles-20130615.1433/multifiles-autoloads.el
@@ -0,0 +1,18 @@
+;;; multifiles-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("multifiles-pkg.el" "multifiles.el") (21250
+;;;;;;  40704 598501 176000))
+
+;;;***
+
+(provide 'multifiles-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; multifiles-autoloads.el ends here
