70e9f81dc26988217ff0bb61f1f1b6786499c1d6
muflax <mail@muflax.com>
2014-03-29 11:53:51 +0000

    reinstall bookmarks+ now that we know it's not to blame; also some helm crap

---
+++ elpa/bookmark+-20140328.1731/bookmark+-pkg.el
@@ -0,0 +1,1 @@
+(define-package "bookmark+" "20140328.1731" "Bookmark+: extensions to standard library `bookmark.el'." 'nil)
---
+++ elpa/helm-flymake-20130716.2244/helm-flymake-autoloads.el
@@ -0,0 +1,29 @@
+;;; helm-flymake-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (helm-flymake) "helm-flymake" "helm-flymake.el"
+;;;;;;  (21302 46030 319611 511000))
+;;; Generated autoloads from helm-flymake.el
+
+(autoload 'helm-flymake "helm-flymake" "\
+helm interface for flymake.
+
+\(fn ARG)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("helm-flymake-pkg.el") (21302 46030 329029
+;;;;;;  393000))
+
+;;;***
+
+(provide 'helm-flymake-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; helm-flymake-autoloads.el ends here
