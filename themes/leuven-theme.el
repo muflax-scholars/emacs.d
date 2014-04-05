be5666f64d05ad9df7ed1bc59d06ca935cd9ce8f
muflax <mail@muflax.com>
2014-04-05 20:22:53 +0100

    doc is more like a comment

--- themes/leuven-theme.el
+++ themes/leuven-theme.el
@@ -120,7 +120,7 @@ Semantic, and Ansi-Color faces are included -- and much more...")
    `(font-lock-comment-delimiter-face ((,class (:foreground "#8D8D84")))) ; #696969
    `(font-lock-comment-face ((,class (:slant italic :foreground "#696969" :background "#f7f7f7")))) ; #696969
    `(font-lock-constant-face ((,class (:foreground "#D0372D"))))
-   `(font-lock-doc-face ((,class ,string)))
+   `(font-lock-doc-face ((,class (:foreground "#696969" :background "#f7f7f7"))))
    `(font-lock-doc-string-face ((,class (:foreground "#008000"))))
    `(font-lock-function-name-face ((,class (:weight normal :foreground "#006699"))))
    `(font-lock-keyword-face ((,class (:bold nil :foreground "#0000FF")))) ; #3654DC
