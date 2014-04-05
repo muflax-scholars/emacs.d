d3a4e9c69e90e57663ffc58f40b55df1d8e8b57e
muflax <mail@muflax.com>
2014-04-05 15:34:43 +0100

    boldly use less stupid folder names

--- init.el
+++ init.el
@@ -1,6 +1,6 @@
 ;; load path; site-lisp stores manually maintained packages
 (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
-    (let* ((my-lisp-dir "~/.emacs.d/site-lisp/")
+    (let* ((my-lisp-dir "~/.emacs.d/local/")
            (default-directory my-lisp-dir))
       (progn
         (setq load-path (cons my-lisp-dir load-path))
@@ -13,6 +13,7 @@
 
 ;; elpa package-repositories
 (require 'package)
+(setq package-user-dir "~/.emacs.d/packages")
 (add-to-list 'package-archives '("tromey" . "http://tromey.com/elpa/") t)
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
