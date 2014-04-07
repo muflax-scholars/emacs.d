8d4f471720f50499fbca59385867383a007f3610
muflax <mail@muflax.com>
2014-04-07 13:56:45 +0100

    install sublimity again, maybe minimap works this time...

--- packages/dash-20140327.116/dash-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; dash-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("dash-pkg.el" "dash.el") (21301 31066
-;;;;;;  591984 614000))
-
-;;;***
-
-(provide 'dash-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; dash-autoloads.el ends here
