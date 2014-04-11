262d09c79f3de5c5698f4566b2c90c1aeb2ee082
muflax <mail@muflax.com>
2014-04-11 03:07:56 +0100

    install a bunch of git stuff because I hate myself

---
+++ packages/egg-20140307.125/egg-pkg.el
@@ -0,0 +1,2 @@
+(define-package "egg" "20140307.125" "Emacs Got Git - Emacs interface to Git" 'nil :keywords
+  '("git" "version control" "release management"))
---
+++ packages/gitty-20121014.742/gitty-pkg.el
@@ -0,0 +1,1 @@
+(define-package "gitty" "20121014.742" "vc-mode extension for fast git interaction" (quote nil))
