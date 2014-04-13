f6abe0c725c52ae86e268d0a79f24239fe776b25
muflax <mail@muflax.com>
2014-04-13 04:49:47 +0100

    package stuff

--- packages/dired-k-20140409.58/dired-k-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "dired-k" "20140409.58" "highlight dired buffer by file size, modified time, git status" (quote ((cl-lib "0.5"))))
--- packages/skewer-mode-20140410.1600/skewer-mode-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "skewer-mode" "20140410.1600" "live browser JavaScript, CSS, and HTML interaction"
-  '((simple-httpd "1.4.0")
-    (js2-mode "20090723")
-    (emacs "24")))
