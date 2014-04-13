7e66096757e051dcca722338fd488a6ebedc4887
muflax <mail@muflax.com>
2014-04-13 07:38:52 +0100

    fullscreen magit

--- init-major-modes.el
+++ init-major-modes.el
@@ -331,7 +331,22 @@
 ;; fancy git interactions
 (setup-lazy '(magit-status) "magit"
   (set-default 'magit-stage-all-confirm nil)
-  (set-default 'magit-unstage-all-confirm nil))
+  (set-default 'magit-unstage-all-confirm nil)
+
+  ;; full screen magit-status
+  (defadvice magit-status (around magit-fullscreen activate)
+    (window-configuration-to-register :magit-fullscreen)
+    ad-do-it
+    (delete-other-windows))
+
+  (defun magit-quit-session ()
+    "Restores the previous window configuration and kills the magit buffer"
+    (interactive)
+    (kill-buffer)
+    (jump-to-register :magit-fullscreen))
+
+  ;; needed because of fullscreen override
+  (define-key magit-status-mode-map (kbd "q") 'magit-quit-session))
 
 (global-set-key (kbd "C-x g") 'magit-status)
 
