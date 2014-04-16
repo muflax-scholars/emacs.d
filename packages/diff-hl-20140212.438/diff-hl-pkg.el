e1679acb88c7726b9d13735b9d60f0dfc32bfc35
muflax <mail@muflax.com>
2014-04-16 20:20:21 +0100

    package upgrade

--- packages/diff-hl-20140212.438/diff-hl-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "diff-hl" "20140212.438" "Highlight uncommitted changes"
-  '((cl-lib "0.2"))
-  :url "https://github.com/dgutov/diff-hl" :keywords
-  '("vc" "diff"))
--- packages/dired-k-20140410.2211/dired-k-autoloads.el
+++
@@ -1,30 +0,0 @@
-;;; dired-k-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (dired-k) "dired-k" "dired-k.el" (21320 29645 782364
-;;;;;;  672000))
-;;; Generated autoloads from dired-k.el
-
-(autoload 'dired-k "dired-k" "\
-Highlighting dired buffer by file size, last modified time, and git status.
-This is inspired by `k' zsh script
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("dired-k-pkg.el") (21320 29645 802099
-;;;;;;  786000))
-
-;;;***
-
-(provide 'dired-k-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; dired-k-autoloads.el ends here
