47dfd2fd8c59b94922979720ff08e65fe187dffe
muflax <mail@muflax.com>
2014-05-18 20:27:29 +0100

    macros

--- init-macro.el
+++ init-macro.el
@@ -9,5 +9,13 @@
 (fset 'm\ clear\ column
    (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([18 124 13 right right 67108896 19 124 13 left left 23 delete right right] 0 "%d")) arg)))
 
+(fset 'm\ add\ annotation
+   (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([5 return 62 32] 0 "%d")) arg)))
+(global-set-key [20 97] 'm\ add\ annotation)
+
+(fset 'm\ add\ explanation
+   (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([5 return 60 32] 0 "%d")) arg)))
+(global-set-key [20 101] 'm\ add\ explanation)
+
 
 (provide 'init-macro)
