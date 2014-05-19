58c51d517ec84bd8f7196e665d0b6eb2f6740906
muflax <mail@muflax.com>
2014-05-19 13:49:12 +0100

    fix typo

--- init-macro.el
+++ init-macro.el
@@ -8,7 +8,7 @@
 ;; selection of useful macros. this should be smarter / automatic / project-specific, but getting that to work is meh.
 (fset 'm\ clear\ column
    (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([18 124 13 right right 67108896 19 124 13 left left 23 delete right right] 0 "%d")) arg)))
-(global-set-key (kbd "C-t c") 'm\ add\ annotation)
+(global-set-key (kbd "C-t c") 'm\ clear\ column)
 
 (fset 'm\ add\ annotation
    (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([5 return 62 32] 0 "%d")) arg)))
