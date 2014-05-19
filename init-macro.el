f46c8c6c7fe8bde262379f822b62f07354040cb1
muflax <mail@muflax.com>
2014-05-19 13:47:27 +0100

    readable keys

--- init-macro.el
+++ init-macro.el
@@ -8,19 +8,21 @@
 ;; selection of useful macros. this should be smarter / automatic / project-specific, but getting that to work is meh.
 (fset 'm\ clear\ column
    (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([18 124 13 right right 67108896 19 124 13 left left 23 delete right right] 0 "%d")) arg)))
+(global-set-key (kbd "C-t c") 'm\ add\ annotation)
 
 (fset 'm\ add\ annotation
    (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([5 return 62 32] 0 "%d")) arg)))
-(global-set-key [20 97] 'm\ add\ annotation)
+(global-set-key (kbd "C-t a") 'm\ add\ annotation)
 
 (fset 'm\ add\ explanation
    (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([5 return 60 32] 0 "%d")) arg)))
-(global-set-key [20 101] 'm\ add\ explanation)
+(global-set-key (kbd "C-t e") 'm\ add\ explanation)
 
 (fset 'm\ next\ empty\ \?
    (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([19 63 32 36 13 134217848 114 101 99 101 110 116 101 114 return] 0 "%d")) arg)))
 (global-set-key [20 110] 'm\ add\ explanation)
-(global-set-key [f13] 'm\ next\ empty\ \?)
+(global-set-key (kbd "C-t n") 'm\ next\ empty\ \?)
+(global-set-key (kbd "<f13>") 'm\ next\ empty\ \?)
 
 
 (provide 'init-macro)
