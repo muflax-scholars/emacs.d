ca09cf9d22941d779b6cbee32e619d1013b9082d
muflax <mail@muflax.com>
2014-07-06 14:30:25 +0100

    bind sticky-window to keys, even though it's not quite usable

--- init-editing.el
+++ init-editing.el
@@ -1049,4 +1049,11 @@ See `flush-lines' or `keep-lines' for behavior of this command."
 (global-set-key (kbd "C-t C-n") 'kmacro-name-last-macro)
 (global-set-key (kbd "C-t C-b") 'kmacro-bind-to-key)
 
+;; sticky windows
+(setup "sticky-windows"
+  (global-set-key (kbd "C-x 0") 'sticky-window-delete-window)
+  (global-set-key (kbd "C-x 1") 'sticky-window-delete-other-windows)
+  (global-set-key (kbd "C-x 9") 'sticky-window-keep-window-visible))
+
+
 (provide 'init-editing)
