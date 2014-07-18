e154b8e7672afde4ca120bfe22e2406655b97499
muflax <mail@muflax.com>
2014-07-18 09:23:30 +0100

    pkg update (for go)

--- packages/racket-mode-20140616.2105/racket-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "racket-mode" "20140616.2105" "Major mode for Racket language." 'nil :url "https://github.com/greghendershott/racket-mode")
---
+++ packages/racket-mode-20140714.1828/racket-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "racket-mode" "20140714.1828" "Major mode for Racket language." 'nil :url "https://github.com/greghendershott/racket-mode")
