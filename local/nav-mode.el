d11124b4af622c8e14c6ad638245739529a7152f
muflax <mail@muflax.com>
2014-07-21 10:43:44 +0100

    some more key experimentation

--- local/nav-mode.el
+++ local/nav-mode.el
@@ -13,10 +13,19 @@
 ;; simple navigation
 (define-key nav-mode-map (kbd "l") 'previous-line)
 (define-key nav-mode-map (kbd "a") 'next-line)
-(define-key nav-mode-map (kbd "v") 'scroll-down-command)
-(define-key nav-mode-map (kbd "c") 'scroll-up-command)
 (define-key nav-mode-map (kbd "i") 'left-char)
 (define-key nav-mode-map (kbd "e") 'right-char)
+
+(define-key nav-mode-map (kbd "u") 'scroll-down-command)
+(define-key nav-mode-map (kbd "o") 'scroll-up-command)
+
+(define-key nav-mode-map (kbd "n") 'other-window)
+(define-key nav-mode-map (kbd "r") (lambda () (interactive) (other-window -1)))
+
+;; editing
+(define-key nav-mode-map (kbd "c") 'kill-and-join-forward)
+(define-key nav-mode-map (kbd "w") 'whole-line-or-region-kill-region)
+(define-key nav-mode-map (kbd "y") 'yank-and-indent)
 
 ;; exit with space
 (defun turn-off-nav-mode ()
