dbdcc8abfb8ae70bb767a0a3b5d691f9d2f4e8f3
muflax <mail@muflax.com>
2014-07-29 09:28:10 +0100

    some number libs for later

---
+++ packages/number-20140719.239/number-autoloads.el
@@ -0,0 +1,18 @@
+;;; number-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("number-pkg.el" "number.el") (21463 23301
+;;;;;;  361121 353000))
+
+;;;***
+
+(provide 'number-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; number-autoloads.el ends here
---
+++ packages/number-20140719.239/number-pkg.el
@@ -0,0 +1,1 @@
+(define-package "number" "20140719.239" "Working with numbers at point." (quote nil))
