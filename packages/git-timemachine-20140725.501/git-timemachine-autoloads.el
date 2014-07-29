d062caa47b623dd66d626e29f24d820f5d305403
muflax <mail@muflax.com>
2014-07-29 10:48:19 +0100

    update

---
+++ packages/git-timemachine-20140725.501/git-timemachine-autoloads.el
@@ -0,0 +1,29 @@
+;;; git-timemachine-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (git-timemachine) "git-timemachine" "git-timemachine.el"
+;;;;;;  (21463 27958 739133 264000))
+;;; Generated autoloads from git-timemachine.el
+
+(autoload 'git-timemachine "git-timemachine" "\
+Enable git timemachine for file of current buffer.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("git-timemachine-pkg.el") (21463 27958
+;;;;;;  772653 436000))
+
+;;;***
+
+(provide 'git-timemachine-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; git-timemachine-autoloads.el ends here
--- packages/racket-mode-20140714.1828/racket-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "racket-mode" "20140714.1828" "Major mode for Racket language." 'nil :url "https://github.com/greghendershott/racket-mode")
