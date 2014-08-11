52c095dcd21bddc8ff9cfa08190e2fca040f2c5c
muflax <mail@muflax.com>
2014-08-11 11:55:10 +0100

    install sunrise commander again, for later

--- init-load-path.el
+++ init-load-path.el
@@ -10,9 +10,10 @@
 ;; elpa package-repositories
 (require 'package)
 (setq package-user-dir "~/.emacs.d/packages")
-(add-to-list 'package-archives '("tromey" . "http://tromey.com/elpa/") t)
+(add-to-list 'package-archives '("tromey"    . "http://tromey.com/elpa/") t)
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("melpa"     . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("SC"        . "http://joseito.republika.pl/sunrise-commander/") t)
 (package-initialize)
 
 ;; some generic aliases that make elisp less painful
---
+++ packages/sunrise-x-loop-3.423/sunrise-x-loop-autoloads.el
@@ -0,0 +1,25 @@
+;;; sunrise-x-loop-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil "sunrise-x-loop" "sunrise-x-loop.el" (21480
+;;;;;;  41174 949935 798000))
+;;; Generated autoloads from sunrise-x-loop.el
+ (eval-after-load 'sunrise-commander '(sr-extend-with 'sunrise-x-loop))
+
+;;;***
+
+;;;### (autoloads nil nil ("sunrise-x-loop-pkg.el") (21480 41174
+;;;;;;  972724 243000))
+
+;;;***
+
+(provide 'sunrise-x-loop-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; sunrise-x-loop-autoloads.el ends here
