2b1df676b60b5f25abc6738b08c89ce6c446e4ac
muflax <mail@muflax.com>
2014-08-22 23:45:42 +0100

    some packages to play with later

---
+++ packages/google-20140330.856/google-autoloads.el
@@ -0,0 +1,18 @@
+;;; google-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("google-pkg.el" "google.el") (21495 51199
+;;;;;;  87775 849000))
+
+;;;***
+
+(provide 'google-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; google-autoloads.el ends here
