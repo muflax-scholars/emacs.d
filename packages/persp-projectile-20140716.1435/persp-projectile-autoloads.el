dd3a38faa210da4a33a7c37928e900b5b62532bc
muflax <mail@muflax.com>
2014-08-26 04:33:21 +0100

    projectile for later

---
+++ packages/persp-projectile-20140716.1435/persp-projectile-autoloads.el
@@ -0,0 +1,18 @@
+;;; persp-projectile-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("persp-projectile-pkg.el" "persp-projectile.el")
+;;;;;;  (21499 64517 113797 765000))
+
+;;;***
+
+(provide 'persp-projectile-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; persp-projectile-autoloads.el ends here
