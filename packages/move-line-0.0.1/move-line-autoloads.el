16f82a4e41ea170c81f104efc0c8ce1ed2b32e37
muflax <mail@muflax.com>
2014-09-10 16:29:30 +0100

    (yet another) alternative for moving lines until I get around to fixing it

---
+++ packages/move-line-0.0.1/move-line-autoloads.el
@@ -0,0 +1,18 @@
+;;; move-line-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("move-line-pkg.el" "move-line.el") (21519
+;;;;;;  22117 397195 996000))
+
+;;;***
+
+(provide 'move-line-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; move-line-autoloads.el ends here
