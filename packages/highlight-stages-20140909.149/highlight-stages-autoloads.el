75b2913dbe475e2037feada2846cdd1a9962b189
muflax <mail@muflax.com>
2014-09-10 16:29:48 +0100

    lisp highlighting for later

---
+++ packages/highlight-stages-20140909.149/highlight-stages-autoloads.el
@@ -0,0 +1,29 @@
+;;; highlight-stages-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (highlight-stages-lisp-initialize) "highlight-stages"
+;;;;;;  "highlight-stages.el" (21519 22117 699808 945000))
+;;; Generated autoloads from highlight-stages.el
+
+(autoload 'highlight-stages-lisp-initialize "highlight-stages" "\
+Setup highlight-stages for Lisp buffers.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("highlight-stages-pkg.el") (21519 22117
+;;;;;;  722148 870000))
+
+;;;***
+
+(provide 'highlight-stages-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; highlight-stages-autoloads.el ends here
---
+++ packages/highlight-stages-20140909.149/highlight-stages-pkg.el
@@ -0,0 +1,1 @@
+(define-package "highlight-stages" "20140909.149" "highlight staged (quasi-quoted) expressions" (quote nil))
