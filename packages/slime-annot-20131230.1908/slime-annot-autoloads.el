7f79d64d6f2613c72184bba98903c7944963f363
muflax <mail@muflax.com>
2014-09-16 02:08:29 +0100

    uninstall unneeded slime packages because of ELPA's dependency problems
    I should really stop using ELPA, but free time etc

--- packages/slime-annot-20131230.1908/slime-annot-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; slime-annot-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("slime-annot-pkg.el" "slime-annot.el")
-;;;;;;  (21504 35198 390369 151000))
-
-;;;***
-
-(provide 'slime-annot-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; slime-annot-autoloads.el ends here
