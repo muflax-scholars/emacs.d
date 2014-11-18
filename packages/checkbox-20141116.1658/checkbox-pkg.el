aef32b1f503df2b1149172e996381d6b2be88dda
muflax <mail@muflax.com>
2014-11-18 16:40:55 +0000

    checkboxes?

---
+++ packages/checkbox-20141116.1658/checkbox-pkg.el
@@ -0,0 +1,1 @@
+(define-package "checkbox" "20141116.1658" "Quick manipulation of textual checkboxes" '((emacs "24") (cl-lib "0.5")) :url "http://github.com/camdez/checkbox.el" :keywords '("convenience"))
