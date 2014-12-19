e2d211ee9f9ec31b5bf5a07cbee55458dbeaa17a
muflax <mail@muflax.com>
2014-12-19 00:21:58 +0000

    package update

--- packages/imenu-anywhere-20141210.1850/imenu-anywhere-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "imenu-anywhere" "20141210.1850" "ido/helm imenu tag selection across all buffers with the same mode" (quote ((cl-lib "0.5"))))
---
+++ packages/rainbow-delimiters-20141218.1252/rainbow-delimiters-pkg.el
@@ -0,0 +1,1 @@
+(define-package "rainbow-delimiters" "20141218.1252" "Highlight brackets according to their depth" (quote nil))
