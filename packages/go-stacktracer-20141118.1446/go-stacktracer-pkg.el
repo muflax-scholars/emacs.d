9177b96c08021a11eabbd2049ae8375f8bbd0063
muflax <mail@muflax.com>
2015-02-16 20:10:29 +0000

    some more updates

--- packages/go-stacktracer-20141118.1446/go-stacktracer-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "go-stacktracer" "20141118.1446" "parse Go stack traces" (quote nil))
--- packages/rust-mode-20150210.1752/rust-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "rust-mode" "20150210.1752" "A major emacs mode for editing Rust source code" (quote nil))
