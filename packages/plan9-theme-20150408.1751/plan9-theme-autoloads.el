efcdb35aecb917d0a00d5a44d68c4bc14f862171
muflax <mail@muflax.com>
2015-04-20 20:37:18 +0100

    new theme to mess around with some time

---
+++ packages/plan9-theme-20150408.1751/plan9-theme-autoloads.el
@@ -0,0 +1,19 @@
+;;; plan9-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "plan9-theme" "plan9-theme.el" (21813 21768
+;;;;;;  871102 172000))
+;;; Generated autoloads from plan9-theme.el
+
+(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; plan9-theme-autoloads.el ends here
---
+++ packages/plan9-theme-20150408.1751/plan9-theme-pkg.el
@@ -0,0 +1,1 @@
+(define-package "plan9-theme" "20150408.1751" "A color theme for Emacs based on Plan9" 'nil :url "https://github.com/john2x/plan9-theme.el")
