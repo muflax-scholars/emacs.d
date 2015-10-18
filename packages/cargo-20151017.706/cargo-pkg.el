fb6b0171cabb87896b0a23100859fcdf86d018ed
Stefan Dorn <mail@muflax.com>
2015-10-18 02:28:03 +0100

    pkg upgrade

---
+++ packages/cargo-20151017.706/cargo-pkg.el
@@ -0,0 +1,7 @@
+(define-package "cargo" "20151017.706" "Emacs Minor Mode for Cargo, Rust's Package Manager."
+  '((emacs "24.3"))
+  :keywords
+  '("tools"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
--- packages/flx-isearch-20150906.1629/flx-isearch-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "flx-isearch" "20150906.1629" "Fuzzy incremental searching for emacs" '((emacs "24") (flx "20140821") (cl-lib "0.5")) :stars '(#("11" 0 2 (font-lock-face paradox-star-face))) :url "https://github.com/pythonnut/flx-isearch" :keywords '("convenience" "search" "flx"))
