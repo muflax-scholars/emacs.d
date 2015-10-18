881b1ce2d1eb2785580b927675592cd73a478809
Stefan Dorn <mail@muflax.com>
2015-10-18 02:33:05 +0100

    stuff

---
+++ packages/ac-geiser-20130928.2347/ac-geiser-autoloads.el
@@ -0,0 +1,31 @@
+;;; ac-geiser-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "ac-geiser" "ac-geiser.el" (22050 63102 109399
+;;;;;;  445000))
+;;; Generated autoloads from ac-geiser.el
+
+(defvar ac-source-geiser '((candidates . ac-source-geiser-candidates) (symbol . "g") (document . ac-geiser-documentation)) "\
+Source for geiser completion")
+
+(autoload 'ac-geiser-setup "ac-geiser" "\
+Add the geiser completion source to the front of `ac-sources'.
+This affects only the current buffer.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("ac-geiser-pkg.el") (22050 63102 141319
+;;;;;;  295000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; ac-geiser-autoloads.el ends here
---
+++ packages/utop-20150806.258/utop-pkg.el
@@ -0,0 +1,1 @@
+(define-package "utop" "20150806.258" "Universal toplevel for OCaml" '((emacs "24")) :stars '(#("195" 0 3 (font-lock-face paradox-star-face))) :url "https://github.com/diml/utop" :keywords '("ocaml" "languages"))
