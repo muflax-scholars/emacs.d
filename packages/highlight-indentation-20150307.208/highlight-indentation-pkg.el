601c9d5c03512462ef4bc28d701e79415cd071a1
Stefan Dorn <mail@muflax.com>
2016-01-03 16:14:10 +0000

    cleanup

--- packages/highlight-indentation-20150307.208/highlight-indentation-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "highlight-indentation" "20150307.208" "Minor modes for highlighting indentation" 'nil :stars '(#("207" 0 3 (font-lock-face paradox-star-face))) :url "https://github.com/antonj/Highlight-Indentation-for-Emacs")
