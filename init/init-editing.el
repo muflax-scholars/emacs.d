f82dcc1a5cafdb6b4ad55654777999f8691a98c7
Stefan Dorn <mail@muflax.com>
2016-01-06 20:03:47 +0000

    fix macro nesting

--- init/init-editing.el
+++ init/init-editing.el
@@ -443,12 +443,12 @@
     (error (message "Invalid expression")
            (insert (current-kill 0)))))
 
-(defun add-lispish-hook (hook)
-  (load-after 'lisp-mode           	
-    (add-hook 'lisp-mode-hook      	hook)
-    (add-hook 'emacs-lisp-mode-hook	hook))
-  (load-after 'racket-mode         	
-    (add-hook 'racket-mode-hook    	hook)))
+(defmacro add-lispish-hook (hook)
+  `(load-after 'lisp-mode           	
+     (add-hook 'lisp-mode-hook      	,hook)
+     (add-hook 'emacs-lisp-mode-hook	,hook))
+  `(load-after 'racket-mode         	
+     (add-hook 'racket-mode-hook    	,hook)))
 
 ;; better s-expression handling
 (require 'adjust-parens)
