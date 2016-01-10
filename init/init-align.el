88afbdba933090a85a641851fcacd01b637d67fa
Stefan Dorn <mail@muflax.com>
2016-01-10 08:54:57 +0000

    cleanup

--- init/init-align.el
+++ init/init-align.el
@@ -10,15 +10,15 @@
         notes-mode
         ))
 
-(elastic-advice-command       	indent-for-tab-command)
-(elastic-advice-command       	literal-tab)
-(elastic-advice-command       	indent-according-to-mode)
-(elastic-advice-command-region	indent-region)
 (elastic-advice-command       	comment-dwim)
-(elastic-advice-command-region	comment-region)
-(elastic-advice-command-region	uncomment-region)
+(elastic-advice-command       	indent-according-to-mode)
+(elastic-advice-command       	indent-for-tab-command)
 (elastic-advice-command       	indent-sexp)
+(elastic-advice-command       	literal-tab)
 (elastic-advice-command       	sp-indent-defun)
+(elastic-advice-command-region	comment-region)
+(elastic-advice-command-region	indent-region)
+(elastic-advice-command-region	uncomment-region)
 
 (load-after 'lispy
   (elastic-advice-command	lispy-comment)
