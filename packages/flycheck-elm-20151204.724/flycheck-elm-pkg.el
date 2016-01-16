faad29736cca405455a564c2388c3f4b9f810dd8
Stefan Dorn <mail@muflax.com>
2016-01-16 01:07:40 +0000

    js nonsense

---
+++ packages/flycheck-elm-20151204.724/flycheck-elm-pkg.el
@@ -0,0 +1,1 @@
+(define-package "flycheck-elm" "20151204.724" "Flycheck support for the elm language" '((flycheck "0.24") (emacs "24.4")) :stars '(#("5" 0 1 (font-lock-face paradox-star-face))) :url "https://github.com/bsermons/flycheck-elm")
