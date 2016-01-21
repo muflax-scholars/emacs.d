2303fe96c106b1facf04a75a8276f9cb6f9c0708
Stefan Dorn <mail@muflax.com>
2016-01-21 17:21:57 +0000

    clean tromey's elpa

--- init/init-packages.el
+++ init/init-packages.el
@@ -5,7 +5,6 @@
 (setq package-user-dir (emacs-d "packages"))
 (setq package-archives nil)
 (add-to-list 'package-archives '("gnu"      	. "https://elpa.gnu.org/packages/")      	t)
-(add-to-list 'package-archives '("tromey"   	. "https://tromey.com/elpa/")            	t)
 (add-to-list 'package-archives '("marmalade"	. "https://marmalade-repo.org/packages/")	t)
 (add-to-list 'package-archives '("melpa"    	. "https://melpa.milkbox.net/packages/") 	t)
 
